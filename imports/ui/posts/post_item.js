c30d6d0318776662f00135d62814f127ba336b20
Travis CI <stephane.langlois@scopyleft.fr>
2016-12-20 00:52:17 +0100

    Move static data to dynamic data

--- imports/ui/posts/post_item.js
+++ imports/ui/posts/post_item.js
@@ -2,9 +2,10 @@ import { Template } from 'meteor/templating'
 import './post_item.html'
 
 Template.postItem.helpers({
-  domain: function() {
-    var a = document.createElement('a')
-    a.href = this.url
+  domain: () => {
+    const post = Template.instance().data
+    const a = document.createElement('a')
+    a.href = post.url
     return a.hostname
   }
 })
--- imports/ui/posts/posts_list.js
+++ imports/ui/posts/posts_list.js
@@ -1,21 +1,8 @@
 import { Template } from 'meteor/templating'
+import { Posts } from '/imports/api/posts.js'
 import './posts_list.html'
 import './post_item.js'
 
-const postsData = [
-  {
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-  }
-]
 Template.postsList.helpers({
-  posts: postsData
+  posts: Posts.find()
 })
