2b3ca65c0c593a68f7d51f4c85ce1025ecfeb6db
Travis CI <stephane.langlois@scopyleft.fr>
2016-12-20 01:03:38 +0100

    Add publish/subscribe methods

--- imports/ui/posts/posts_list.js
+++ imports/ui/posts/posts_list.js
@@ -1,7 +1,12 @@
 import { Template } from 'meteor/templating'
 import { Posts } from '/imports/api/posts.js'
+import { Meteor } from 'meteor/meteor'
 import './posts_list.html'
 import './post_item.js'
+
+Template.postsList.onCreated(() => {
+  Meteor.subscribe('posts')
+})
 
 Template.postsList.helpers({
   posts: Posts.find()
